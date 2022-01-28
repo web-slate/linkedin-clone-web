04bf487f4b47a5a1e49fb386a770df58734394d2
Veera Prasad D <veera.prasad0@experian.com>
2022-01-28 10:24:20 +0530

    Built slim react-chef app.

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+
+const webpack = require("webpack");
+const path = require("path");
+const PACKAGE = require("./package.json");
+
+// WebPack Plugins.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /.(js)$/,
+        exclude: [/node_modules/],
+        use: ["babel-loader"],
+      },
+      {
+        test: /.svg$/,
+        use: ["@svgr/webpack", "file-loader"],
+      },
+      {
+        test: /.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+    alias: {
+      "@linkedinWeb/images": path.resolve(
+        __dirname,
+        "src",
+        "static",
+        "assets",
+        "images"
+      ),
+      "@linkedinWeb/components": path.resolve(
+        __dirname,
+        "src",
+        "components"
+      ),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "linkedinWeb.js",
+    chunkFilename: "[name].js",
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+
+    new webpack.EnvironmentPlugin({
+      VERSION: PACKAGE.version,
+    }),
+
+    // Take Reference of HTML File.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "src/static/index.html"),
+      APP_ROOT_ID: 'linkedinWeb',
+      APP_VERSION: PACKAGE.version
+    }),
+
+    // Copy all Assets, Icons to public Folder.
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/static/images", to: "images" },
+      ],
+    }),
+  ],
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    static: {
+      directory: "./src/static",
+    },
+    hot: true,
+    port: 7000,
+    proxy: {
+      "/api": "http://YOUR_API_URL:9000",
+    },
+  },
+};    
