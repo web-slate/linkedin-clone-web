3f3ecc33a6df63960063c2a22be825840253735b
Veera Prasad Dagudu <veeraprasadd@gmail.com>
2022-01-30 16:04:49 +0530

    tech(base): initial react-chef app configuration files (#3)
    Co-authored-by: Veera Prasad D <veera.prasad0@experian.com>

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Components.
+import { ErrorHandler } from '@linkedinWeb/components'
+
+function App() {
+  return (
+    <>
+      <ErrorHandler>
+        <h1>Welcome to React Chef Slim App</h1>
+      </ErrorHandler>
+    </>
+  )
+}
+
+export default App
