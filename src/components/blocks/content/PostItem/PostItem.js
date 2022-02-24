fabfc9deb5fe608eb0dbd044157ccee98c876d12
govindarajkp <govindaraj.k.p@gmail.com>
2022-02-24 18:03:42 +0530

    tech(component/base) : dividing post item component

--- src/components/blocks/content/PostItem/PostItem.js
+++ src/components/blocks/content/PostItem/PostItem.js
@@ -1,19 +1,25 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import PostForm from '../PostForm'
 import imageSrc from '../../../../static/images/jp.png';
 import useGetPostList from '../../../services/hooks/useGetPostList';
 import { FetchView } from "react-camouflage";
+import PostAuthourInfo from './postAuthorInfo';
+import PostContent from './postContent';
+import PostInteractions from './postInteractions';
 
 function PostItem() {
   const { isLoading, serverError, postItems } = useGetPostList();
-  const postListStatusMapping =  {
+  const postListStatusMapping = {
     success: postItems,
     error: serverError,
     loading: isLoading
   };
+  const renderCounter = useRef(0);
+  renderCounter.current = renderCounter.current + 1;
 
   return <>
+
     <div id="main-wrapper">
       <main id="main-section">
         <PostForm></PostForm>
@@ -42,160 +48,15 @@ function PostItem() {
             <span className="fas fa-sort-down"></span>
           </button>
         </div>
-        <article>
-          <div id="post-author">
-            <a href="#">
-              <div>
-                <img
-                  src="https://avatars.githubusercontent.com/u/16048888?s=96&v=4"
-                  alt=""
-                />
-                <div>
-                  <div>
-                    <strong id="post-author-name"
-                    >Govindaraj K    </strong>
-                    <span>
-                      <span>&nbsp;·&nbsp;</span>
-                      1st</span
-                    >
-                  </div>
-                  <span
-                  >Web Developer at Jp Morgan</span
-                  >
-                  <span>12h</span>
-                </div>
-              </div>
-            </a>
-            <div>
-              <span className="fas fa-circle"></span>
-              <span className="fas fa-circle"></span>
-              <span className="fas fa-circle"></span>
-            </div>
-          </div>
-          <div id="post-data">
-            <p>
-              Yesterday I had a conversation with
-              <span> Yuvraj </span>, Front-End
-              Developer, and found out that he cloned LinkedIn
-              layout using HTML/CSS. I think you should check
-              this out ;)
-            </p>
-            <p id="post-translation">
-              <button>See translation</button>
-            </p>
 
-          </div>
-          <div id="post-interactions">
-            <div id="interactions-amount">
-              <span
-                id="like-icon"
-                className="fas fa-thumbs-up fa-flip-horizontal"
-              ></span>
-              <span
-                id="heart-icon"
-                className="fas fa-heart"
-              ></span>
-              <span id="amount-info"
-              >23 <span>&nbsp;·&nbsp;</span> 4
-                Comments</span
-              >
-            </div>
-            <div id="interactions-btns">
-              <button>
-                <span
-                  className="far fa-thumbs-up fa-flip-horizontal"
-                ></span>
-                <span>Like</span>
-              </button>
-              <button>
-                <span
-                  className="far fa-comment-dots fa-flip-horizontal"
-                ></span>
-                <span>Comment</span>
-              </button>
-              <button>
-                <span className="far fa-share-square"></span>
-                <span>Share</span>
-              </button>
-            </div>
-          </div>
+        {/* Can be implemented on map for listing the post items */}
+        <article>
+          {renderCounter.current}
+          <PostAuthourInfo />
+          <PostContent />
+          <PostInteractions />
         </article>
 
-        <article>
-          <div id="post-author">
-            <a href="#">
-              <div>
-                <img
-                  src={imageSrc}
-
-                  alt=""
-                />
-                <div>
-                  <div>
-                    <strong id="post-author-name"
-                    >JP    </strong>
-                    <span>
-                      <span>&nbsp;·&nbsp;</span>
-                      1st</span
-                    >
-                  </div>
-                  <span
-                  >Web Developer at Facebook</span
-                  >
-                  <span>12h</span>
-                </div>
-              </div>
-            </a>
-            <div>
-              <span className="fas fa-circle"></span>
-              <span className="fas fa-circle"></span>
-              <span className="fas fa-circle"></span>
-            </div>
-          </div>
-          <div id="post-data">
-            <p>
-              According to the report, the continued political instability caused the forecast for the investment segment in 2022 to be sluggish. Sluggish recovery is also expected for the retail and office sectors, leaving only the residential market for bright recovery thanks to the property-related incentives under Budget 2022.
-            </p>
-            <p id="post-translation">
-              <button>See translation</button>
-            </p>
-
-          </div>
-          <div id="post-interactions">
-            <div id="interactions-amount">
-              <span
-                id="like-icon"
-                className="fas fa-thumbs-up fa-flip-horizontal"
-              ></span>
-              <span
-                id="heart-icon"
-                className="fas fa-heart"
-              ></span>
-              <span id="amount-info"
-              >10025 <span>&nbsp;·&nbsp;</span> 4521
-                Comments</span
-              >
-            </div>
-            <div id="interactions-btns">
-              <button>
-                <span
-                  className="far fa-thumbs-up fa-flip-horizontal"
-                ></span>
-                <span>Like</span>
-              </button>
-              <button>
-                <span
-                  className="far fa-comment-dots fa-flip-horizontal"
-                ></span>
-                <span>Comment</span>
-              </button>
-              <button>
-                <span className="far fa-share-square"></span>
-                <span>Share</span>
-              </button>
-            </div>
-          </div>
-        </article>
 
       </main>
     </div>
