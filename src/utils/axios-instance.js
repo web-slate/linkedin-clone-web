84162624833ad962dcebf4552bbba23b8d971ae8
Venkat.R <ramsunvtech@gmail.com>
2022-02-24 18:53:07 +0800

    fix(api/cors): add default cors headers

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -3,8 +3,18 @@ import axios from 'axios';
 // TODO: get BASE_URL .env file
 const BASE_URL = 'https://linkedincloneapp01.herokuapp.com/api';
 
-const AxiosInstance = axios.create({
-    baseURL: BASE_URL,
-  });
+const defaultHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': 'true',
+}
 
-export default AxiosInstance;
+const Axios = () => {
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: defaultHeaders,
+  })
+
+  return axiosInstance
+}
+
+export default Axios;
