59af75e7bd39385ae0d071f88a2aed14c9f69667
Venkat.R <ramsunvtech@gmail.com>
2022-12-08 21:14:48 +0530

    feat(post-item): add post item header

---
+++ react/src/components/blocks/content/PostItem/Header/PostAuthorInfo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function PostAuthourInfo() {
+  return (
+    <>
+      <div id="post-author">
+        <a href="#">
+          <div>
+            <img src="https://avatars.githubusercontent.com/u/16048888?s=96&v=4" alt="" />
+            <div>
+              <div>
+                <strong id="post-author-name">Govindaraj K</strong>
+                <span>
+                  <span>&nbsp;·&nbsp;</span>1st
+                </span>
+              </div>
+              <span>Web Developer at Jp Morgan</span>
+              <span>12h</span>
+            </div>
+          </div>
+        </a>
+        <div>
+          <span className="fas fa-circle"></span>
+          <span className="fas fa-circle"></span>
+          <span className="fas fa-circle"></span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default PostAuthourInfo
