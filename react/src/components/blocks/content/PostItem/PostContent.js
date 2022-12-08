4ee3a3c25fff07cb743a839f48b6ca86e15555d8
Venkat.R <ramsunvtech@gmail.com>
2022-12-08 20:17:16 +0530

    tech: Refactor PostContent.js
    tech: Refactor PostContent.js

--- react/src/components/blocks/content/PostItem/PostContent.js
+++ react/src/components/blocks/content/PostItem/PostContent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function PostContent() {
+
   return (
     <>
       <div id="post-data">
@@ -15,6 +16,7 @@ function PostContent() {
       </div>
     </>
   )
+
 }
 
 export default PostContent
