dd4f6e2fbd8da7990e8cfd1f1012c8b5b5a2b208
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 20:12:42 +0530

    tech(webpack): add hooks alias

--- react/webpack/config.js
+++ react/webpack/config.js
@@ -1,6 +1,6 @@
 // Helpers.
 const { HOT_MODULE_PLUGIN, ENVIRONMENT_PLUGIN, HTML_PLUGIN, COPY_PLUGIN } = require('./plugins')
-const { IMAGE_ALIAS, COMPONENT_ALIAS, UTIL_ALIAS } = require('./alias')
+const { IMAGE_ALIAS, COMPONENT_ALIAS, HOOKS_ALIAS, UTIL_ALIAS } = require('./alias')
 const { CSS_LOADER, JS_LOADER, SVEG_LOADER, FILE_LOADER } = require('./loaders')
 
 const output = require('./output')
