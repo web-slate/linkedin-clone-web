3c856a37b7cf695d0ba95dd11fbabc0eaeb03153
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 16:47:14 +0530

    fix(webpack/build): export to module.exports

--- react/webpack/plugins.js
+++ react/webpack/plugins.js
@@ -7,13 +7,13 @@ const CopyPlugin = require('copy-webpack-plugin')
 
 const PACKAGE = require('../package.json')
 
-export const HOT_MODULE_PLUGIN = new webpack.HotModuleReplacementPlugin()
+const HOT_MODULE_PLUGIN = new webpack.HotModuleReplacementPlugin()
 
-export const ENVIRONMENT_PLUGIN = new webpack.EnvironmentPlugin({
+const ENVIRONMENT_PLUGIN = new webpack.EnvironmentPlugin({
   VERSION: PACKAGE.version,
 })
 
-export const HTML_PLUGIN = new HtmlWebpackPlugin({
+const HTML_PLUGIN = new HtmlWebpackPlugin({
   inject: true,
   favicon: './src/static/favicon.svg',
   template: path.resolve(__dirname, 'src/static/index.html'),
@@ -22,6 +22,13 @@ export const HTML_PLUGIN = new HtmlWebpackPlugin({
   APP_VERSION: PACKAGE.version,
 })
 
-export const COPY_PLUGIN = new CopyPlugin({
+const COPY_PLUGIN = new CopyPlugin({
   patterns: [{ from: './src/static/images', to: 'images' }],
 })
+
+module.exports = {
+  HOT_MODULE_PLUGIN,
+  ENVIRONMENT_PLUGIN,
+  HTML_PLUGIN,
+  COPY_PLUGIN
+}
