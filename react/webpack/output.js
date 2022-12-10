e90c9874ca1ed41414983a5e57205ab2f53f2cca
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 21:13:34 +0530

    fix(webpack/demo): update output directory

--- react/webpack/output.js
+++ react/webpack/output.js
@@ -3,7 +3,7 @@ const path = require('path')
 const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production'
 
 module.exports = {
-  path: path.resolve(__dirname, '../public'),
+  path: path.resolve(__dirname, '../../public'),
   publicPath: !isProduction ? '/' : '',
   filename: 'linkedin-clone.js',
   chunkFilename: '[name].js',
