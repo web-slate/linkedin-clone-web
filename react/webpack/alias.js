43a478c7788400bdadd57e8358c6036017f65abc
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 19:54:33 +0530

    tech(webpack/alias): move all values to variables

--- react/webpack/alias.js
+++ react/webpack/alias.js
@@ -1,19 +1,36 @@
 const path = require('path')
 
+const SOURCE_DIR = 'src'
+const STATIC_DIR = 'static'
+const IMAGE_DIR = 'images'
+const COMPONENT_DIR = 'components'
+const HOOKS_DIR = 'services/hooks'
+const UTIL_DIR = 'utils'
+
+const STATIC_ALIAS = {
+  '@/static': path.resolve(__dirname, '..', SOURCE_DIR, STATIC_DIR),
+}
+
 const IMAGE_ALIAS = {
-  '@/images': path.resolve(__dirname, '..', 'src', 'static', 'images'),
+  '@/images': path.resolve(__dirname, '..', SOURCE_DIR, STATIC_DIR, IMAGE_DIR),
 }
 
 const COMPONENT_ALIAS = {
-  '@/components': path.resolve(__dirname, '..', 'src', 'components'),
+  '@/components': path.resolve(__dirname, '..', SOURCE_DIR, COMPONENT_DIR),
+}
+
+const HOOKS_ALIAS = {
+  '@/components': path.resolve(__dirname, '..', SOURCE_DIR, HOOKS_DIR),
 }
 
 const UTIL_ALIAS = {
-  '@/utils': path.resolve(__dirname, '..', 'src', 'utils'),
+  '@/utils': path.resolve(__dirname, '..', SOURCE_DIR, UTIL_DIR),
 }
 
 module.exports = {
+  STATIC_ALIAS,
   IMAGE_ALIAS,
   COMPONENT_ALIAS,
+  HOOKS_ALIAS,
   UTIL_ALIAS
 }
