dd5a8da2ca92d98eeb8a61691ca27919705a55d3
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 10:06:40 +0530

    tech(clean-up): replace @linkedinWeb to @ in Profile.js

--- react/src/modules/Profile/Profile.js
+++ react/src/modules/Profile/Profile.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 // Layout
-import MainLayout from '@linkedinWeb/components/layouts/MainLayout'
+import MainLayout from '@/components/layouts/MainLayout'
 
 // Components
-import ProfileIntro from '@linkedinWeb/components/blocks/Profile/ProfileIntro'
+import ProfileIntro from '@/components/blocks/Profile/ProfileIntro'
 import ExperienceSummaryCard from '../../components/blocks/Profile/ExperienceSummaryCard/ExperienceSummaryCard'
 
 const Profile = () => {
