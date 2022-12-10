183839d7105305b42a29f7f271f65249542a2585
Venkat.R <ramsunvtech@gmail.com>
2022-12-10 11:48:01 +0530

    tech(locator-js): enable setupLocatorUI

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './i18n/config'
+import setupLocatorUI from "@locator/runtime";
 
 import App from './App'
+import './i18n/config'
 import './static/styles.css'
+
+if (process.env.NODE_ENV === "development") {
+  setupLocatorUI();
+}
 
 ReactDOM.render(<App />, document.getElementById('linkedin-clone-web'))
